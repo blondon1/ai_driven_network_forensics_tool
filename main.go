c8eacfeac008d74014e47ea81db5aaa790dd47d9
Bryan <bryanlondres5@gmail.com>
2024-08-09 18:14:51 -0400

    Added real-time analysis framework with real_time.go and integrated it into main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/analysis"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/data_ingestion"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/preprocessing"
+	"github.com/blondon1/ai_driven_network_forensics_tool/src/real_time_analysis"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/reporting"
 )
 
@@ -17,6 +18,7 @@ func main() {
 	for packet := range packets {
 		preprocessing.PreprocessPacket(packet)
 		analysis.AnalyzePacket(packet)
+		real_time_analysis.AnalyzeInRealTime(packet)
 		reporting.GenerateReport(packet)
 	}
 }
---
+++ src/real_time_analysis/real_time.go
@@ -0,0 +1,11 @@
+package real_time_analysis
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+)
+
+func AnalyzeInRealTime(packet gopacket.Packet) {
+	// Placeholder for real-time analysis logic
+	fmt.Println("Real-time analysis of packet:", packet)
+}
