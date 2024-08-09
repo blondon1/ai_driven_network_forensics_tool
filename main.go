c8099da38b10dfdf1f211094b7e151bcc9a3e403
Bryan <bryanlondres5@gmail.com>
2024-08-09 18:16:14 -0400

    Added system configuration with system_config.yaml, model configuration with model_config.yaml, and updated main.go to load config

--- main.go
+++ main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/analysis"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/data_ingestion"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/preprocessing"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/real_time_analysis"
 	"github.com/blondon1/ai_driven_network_forensics_tool/src/reporting"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+type SystemConfig struct {
+	NetworkInterface string `yaml:"network_interface"`
+	LogFilePath      string `yaml:"log_file_path"`
+}
+
+func loadConfig() (SystemConfig, error) {
+	var config SystemConfig
+	file, err := os.Open("config/system_config.yaml")
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return config, err
+	}
+
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
+}
+
 func main() {
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
 	packets := make(chan gopacket.Packet)
 
 	go func() {
-		data_ingestion.CapturePackets("eth0", packets)
+		data_ingestion.CapturePackets(config.NetworkInterface, packets)
 	}()
 
 	for packet := range packets {
