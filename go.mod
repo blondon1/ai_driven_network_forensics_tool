3194ca3164d70916d6fd04a7950d16438e7282bc
Bryan <bryanlondres5@gmail.com>
2024-08-09 17:56:19 -0400

    initialized golang project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blondon1/ai_driven_network_forensics_tool
+
+go 1.22.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("AI-Driven Network Forensics Tool Initialized")
+}
